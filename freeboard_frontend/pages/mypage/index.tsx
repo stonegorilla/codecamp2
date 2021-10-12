c8ec8e9969a62e8d1b360eee98e4050b2af1d09e
Dolwangsung <tjwnrhkd123456@gmail.com>
2021-10-12 22:49:01 +0900

    mypageindex

--- freeboard_frontend/pages/mypage/index.tsx
+++ freeboard_frontend/pages/mypage/index.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import withAuth from "../../src/components/commons/hocs/withAuth";
 import MyPage from "../../src/components/units/mypage/MyPage.container";
 
 function MyPagePage() {
+  const [isActive01, setIsActive01] = useState(true);
+  const [isActive02, setIsActive02] = useState(false);
+  const [isActive03, setIsActive03] = useState(false);
+
+  const onClickMyItem = () => {
+    setIsActive01(true);
+    setIsActive02(false);
+    setIsActive03(false);
+  };
+  const onClickMyPoint = () => {
+    setIsActive01(false);
+    setIsActive02(true);
+    setIsActive03(false);
+  };
+  const onClickMyProfile = () => {
+    setIsActive01(false);
+    setIsActive02(false);
+    setIsActive03(true);
+  };
   return <MyPage />;
 }
 
