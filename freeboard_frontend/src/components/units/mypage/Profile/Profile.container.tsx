3918a4732450926ff97dbbc3d26acd0fcabe8726
Dolwangsung <tjwnrhkd123456@gmail.com>
2021-10-13 15:14:47 +0900

    mypagefolderchange

--- freeboard_frontend/src/components/units/mypage/Profile/Profile.container.tsx
+++ freeboard_frontend/src/components/units/mypage/Profile/Profile.container.tsx
@@ -1,16 +1,21 @@
 import { useMutation } from "@apollo/client";
 import ProfileUI from "./Profile.presenter";
-import { CHANGE_PASSWORD } from "./Profile.queries";
+import { CHANGE_PASSWORD, UPDATE_USER } from "./Profile.queries";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaPassword, schemaUser } from "./Profile.validations";
 export default function Profile(){
-    const { register, handleSubmit, formState } = useForm({
+    const { register : registerPwd , handleSubmit : handleSubmitPwd, formState : formStatePwd } = useForm({
         mode: "onChange",
         resolver: yupResolver(schemaPassword),
       });
+      const { register :registerUser, handleSubmit :handleSubmitUser, formState : formStateUser } = useForm({
+        mode: "onChange",
+        resolver: yupResolver(schemaUser),
+      });
       const [changepassword] = useMutation(CHANGE_PASSWORD);
-    
+      const [changeuser] = useMutation(UPDATE_USER)
+
       async function onSubmitPassword(data) {
         try {
           await changepassword({
@@ -23,11 +28,31 @@ export default function Profile(){
           alert(error.message);
         }
       }
+
+      async function onSubmitUser(data) {
+        try {
+          await changeuser({
+            variables: {
+              updateUserInput: {
+                name: data.name,
+              },
+            },
+          });
+          alert("회원정보변경성공");
+        } catch (error) {
+          alert(error.message);
+        }
+      }
     return <ProfileUI 
-    onSubmit={onSubmitPassword}
-    register={register}
-    handleSubmit={handleSubmit}
-    errors={formState.errors}
-    isActive={formState.isValid}
+    onSubmitPassword={onSubmitPassword}
+    onSubmitUser = {onSubmitUser}
+    registerPwd={registerPwd}
+    registerUser = {registerUser}
+    handleSubmitPwd={handleSubmitPwd}
+    handleSubmitUser = {handleSubmitUser}
+    errorsPwd={formStatePwd.errors}
+    errorsUser = {formStateUser.errors}
+    isActivePwd={formStatePwd.isValid}
+    isActiveUser = {formStateUser.isValid}
     />
 }
--- freeboard_frontend/src/components/units/mypage/Profile/Profile.presenter.tsx
+++ freeboard_frontend/src/components/units/mypage/Profile/Profile.presenter.tsx
@@ -4,20 +4,36 @@ import {Wrapper} from './Profile.styles'
 
 export default function ProfileUI(props){
     return (
+        <>
         <Wrapper>
-            <form onSubmit={props.handleSubmit(props.onSubmitPassword)}>
+            <form onSubmit={props.handleSubmitPwd(props.onSubmitPassword)}>
           <Inputmain
             inputName="비밀번호"
             type="text"
-            register={{ ...props.register("password") }}
-            errorMessage={props.errors.password?.message}
+            register={{ ...props.registerPwd("password") }}
+            errorMessage={props.errorsPwd.password?.message}
           />
           <Button01
             buttonName="비밀번호 변경"
             type="submit"
-            isActive={props.isActive}
+            isActive={props.isActivePwd}
+          />
+        </form>
+        <form onSubmit={props.handleSubmitUser(props.onSubmitUser)}>
+          <Inputmain
+            inputName="이름변경"
+            type="text"
+            register={{ ...props.registerUser("name") }}
+            errorMessage={props.errorsUser.name?.message}
+          />
+          <Button01
+            buttonName="회원정보 변경"
+            type="submit"
+            isActive={props.isActiveUser}
           />
         </form>
         </Wrapper>
+
+        </>
     )
 }
