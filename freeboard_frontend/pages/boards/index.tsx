dd15257d28170cf6a0966137e1708cc766aca512
Dolwangsung <tjwnrhkd123456@gmail.com>
2021-10-27 21:52:28 +0900

    change

--- freeboard_frontend/pages/boards/index.tsx
+++ freeboard_frontend/pages/boards/index.tsx
@@ -1,18 +1,5 @@
+import BoardList from "../../src/components/units/board/list/BoardList.container";
 
-import BoardList from '../../src/components/units/board/list/BoardList.container'
-
-
-
-
-export default function MapPage3(){
-
-   
-
-    return (
-       
-        <BoardList/>
-       
-       
-       
-    )
+export default function MapPage3() {
+  return <BoardList />;
 }
