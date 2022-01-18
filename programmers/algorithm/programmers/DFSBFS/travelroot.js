daa7b91e73569951a3ea98546d9c7d97f7ed23fb
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-01-18 17:42:46 +0900

    travelroot

--- programmers/algorithm/programmers/DFSBFS/travelroot.js
+++ programmers/algorithm/programmers/DFSBFS/travelroot.js
@@ -22,19 +22,79 @@
 //   return answer;
 // }
 
-function solution2(tickets) {
-  var answer = [];
-  if (tickets.length === 1 && tickets[0][0] === "ICN") {
-    return tickets[0];
-  } else if (tickets.length === 1) {
-    return [];
-  } else {
-  }
+// function solution2(tickets) {
+//   let ticketssorted = tickets.sort();
+//   let answer = aaa("ICN", ticketssorted, [["ICN"]]);
+//   console.log(answer);
+//   return answer;
+// }
 
-  return solution2();
+// function aaa(start, tickets, array) {
+//   let newstart = "";
+//   let newtickets = [];
+//   for (let i = 0; i < tickets.length; i++) {
+//     if (tickets.length === 1) {
+//       return [[...array, tickets[0][1]]];
+//     }
+//     if (tickets[i][0] === start) {
+//       array.push(tickets[i][1]);
+//       newstart = tickets[i][1];
+//       tickets.splice(i, 1);
+//     }
+//   }
+//   newtickets = JSON.parse(JSON.stringify(tickets));
+//   let newarray = JSON.parse(JSON.stringify(array));
+//   console.log("aaa", newstart);
+//   console.log("bbb", newtickets);
+
+//   return [...aaa(newstart, newtickets, newarray)];
+// }
+
+// solution2([
+//   ["ICN", "SFO"],
+//   ["ICN", "ATL"],
+//   ["SFO", "ATL"],
+//   ["ATL", "ICN"],
+//   ["ATL", "SFO"],
+// ]);
+
+function solution3(tickets) {
+  let answer = [];
+  let ticketssorted = tickets.sort();
+  answer = bbb("ICN", ticketssorted, tickets.length - 1);
+
+  return answer[0];
 }
 
-solution2([
+function bbb(start, tickets, itr) {
+  let routearrays = [];
+
+  if (itr === 0) {
+    return [[tickets[0][1]]];
+  }
+  for (let i = 0; i < tickets.length; i++) {
+    if (tickets[i][0] === start) {
+      routearrays = [
+        ...routearrays,
+        ...bbb(
+          tickets[i][1],
+          tickets.filter((_, index) => index !== i),
+          itr - 1
+        ).map((data) => [start, ...data]),
+      ];
+    }
+  }
+
+  return routearrays;
+}
+
+solution3([
+  ["ICN", "CNA"],
+  ["CNA", "JPN"],
+  ["CNA", "TTR"],
+]);
+
+solution3([
   ["ICN", "SFO"],
   ["ICN", "ATL"],
   ["SFO", "ATL"],
--- programmers/practice/u.js
+++ programmers/practice/u.js
@@ -1,11 +1,15 @@
-const aaa = "2021-11-12";
+// const aaa = "2021-11-12";
 
+// console.log(aaa);
+
+// if (/^(19|20)\d{2}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[0-1])$/.test(aaa)) {
+//   console.log("앙기모띠");
+// }
+
+// let ccc = [8, 9, 10, 15, 12, 19, 24];
+// let c = ccc.find((r) => Math.abs(r - 2) <= 1);
+// console.log(c);
+
+const aaa = ["a", "b", "c"];
+const bbb = aaa.splice(1, 1);
 console.log(aaa);
-
-if (/^(19|20)\d{2}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[0-1])$/.test(aaa)) {
-  console.log("앙기모띠");
-}
-
-let ccc = [8, 9, 10, 15, 12, 19, 24];
-let c = ccc.find((r) => Math.abs(r - 2) <= 1);
-console.log(c);
