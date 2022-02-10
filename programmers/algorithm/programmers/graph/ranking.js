7777c2cbdde94288a8feffccf6b26746d739b955
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-02-10 21:02:26 +0900

    wow

--- programmers/algorithm/programmers/graph/ranking.js
+++ programmers/algorithm/programmers/graph/ranking.js
@@ -1,14 +1,16 @@
 function solution(n, results) {
   var answer = 0;
-
+  console.log(winnumbers(4, results, [4]));
   for (let i = 1; i <= n; i++) {
+    console.log(winnumbers(i, results, [i]).length);
     if (
-      winnumbers(i, results).length + losenumbers(i, results).length ===
+      winnumbers(i, results, [i]).length +
+        losenumbers(i, results, [i]).length ===
       n - 1
     )
       answer++;
   }
-  console.log(answer);
+  // console.log(answer);
   return answer;
 }
 
@@ -16,27 +18,26 @@ function winnumbers(number, results2, duparr) {
   let array = [];
   let results3 = results2.filter((data) => data[0] !== number);
   for (var i of results2) {
-    if (i[0] === number && !array.includes(i[1])) {
+    if (i[0] === number && !duparr.includes(i[1])) {
       array.push(i[1]);
-      array = array.concat(winnumbers(i[1], results3));
+      duparr.push(i[1]);
+      array = array.concat(winnumbers(i[1], results3, duparr));
+      duparr = [...array];
     }
   }
-  const set = new Set(array);
-  array = [...set];
+
   return array;
 }
 
 function losenumbers(number, results2, duparr) {
   let array = [];
-
-  let results3 = results2.filter((data) => data[1] !== number);
-
+  let results3 = results2.filter((data) => data[0] !== number);
   for (var i of results2) {
-    if (i[1] === number && !array.includes(i[0])) {
+    if (i[1] === number && !duparr.includes(i[0])) {
       array.push(i[0]);
-      array = array.concat(losenumbers(i[0], results3));
-      const set = new Set(array);
-      array = [...set];
+      duparr.push(i[0]);
+      array = array.concat(losenumbers(i[0], results3, duparr));
+      duparr = [...array];
     }
   }
 
@@ -51,14 +52,14 @@ solution(5, [
   [2, 5],
 ]);
 
-solution(3, [
-  [3, 1],
-  [3, 2],
-  [1, 2],
-]);
+// solution(3, [
+//   [3, 1],
+//   [3, 2],
+//   [1, 2],
+// ]);
 
-solution(4, [
-  [1, 2],
-  [2, 3],
-  [1, 4],
-]);
+// solution(4, [
+//   [1, 2],
+//   [2, 3],
+//   [1, 4],
+// ]);
