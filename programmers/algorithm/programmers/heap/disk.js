e8ed412fda94095856cb4ad9a410cdcaa6200922
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-03-06 23:42:18 +0900

    disk

--- programmers/algorithm/programmers/heap/disk.js
+++ programmers/algorithm/programmers/heap/disk.js
@@ -1,4 +1,19 @@
 function solution(jobs) {
-  var answer = 0;
+  let answer = 0;
+  let queue = [];
+  let t = 0;
+  jobs = jobs.sort((a, b) => a[0] - b[0]);
+  for(let i=0; i<jobs.length; i++){
+    queue.push(jobs[i])
+    for(let j=0; j<queue.length; j++){
+      while()
+    }
+  }
   return answer;
 }
+
+solution([
+  [0, 3],
+  [1, 9],
+  [2, 6],
+]);
