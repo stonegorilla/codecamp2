1246252efdaa7eddbbd451539d5b59b13de39692
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-05-30 18:41:52 +0900

    menurenewal

--- programmers/algorithm/programmers/level2/coupledelete.js
+++ programmers/algorithm/programmers/level2/coupledelete.js
@@ -1,25 +1,33 @@
-function solution(s)
-{
-    let arr = [];
-    let arrre = [];
+// function solution(s)
+// {
+//     let arr = [];
+//     let arrre = [];
 
-    for(let i=0; i<s.length; i++){
-        
-        if(arrre.length === 0){
-            if(!arr.length) arr.push(s[i]);
-            else if(s[i] !== arr[arr.length-1]) arr.push(s[i]);
-            else{
-                arrre.push(s[i])
-            }
-        }else{
-            if(s[i])
-        }
-        
+//     for(let i=0; i<s.length; i++){
 
+//         if(arrre.length === 0){
+//             if(!arr.length) arr.push(s[i]);
+//             else if(s[i] !== arr[arr.length-1]) arr.push(s[i]);
+//             else{
+//                 arrre.push(s[i])
+//             }
+//         }else{
+//             if(s[i])
+//         }
 
+//     }
+
+//     return answer;
+// }
+
+function solution(s) {
+  let array = [];
+  for (let i = 0; i < s.length; i++) {
+    if (array[array.length - 1] !== s[i]) array.push(s[i]);
+    else {
+      array.pop();
     }
+  }
 
-    
-    
-    return answer;
+  return array.length === 0 ? 1 : 0;
 }
