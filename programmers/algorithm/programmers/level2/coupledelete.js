a446423ec7e7fd505dfa56552d1c19127dd3dd77
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-06-01 23:04:06 +0900

    test17

--- programmers/algorithm/programmers/level2/coupledelete.js
+++ programmers/algorithm/programmers/level2/coupledelete.js
@@ -1,25 +1,46 @@
-function solution(s)
-{
-    let arr = [];
-    let arrre = [];
-
-    for(let i=0; i<s.length; i++){
-        
-        if(arrre.length === 0){
-            if(!arr.length) arr.push(s[i]);
-            else if(s[i] !== arr[arr.length-1]) arr.push(s[i]);
-            else{
-                arrre.push(s[i])
-            }
-        }else{
-            if(s[i])
+function solution(s) {
+  let arr = [];
+  let arrre = [];
+  let testindex = -1;
+  for (let i = 0; i < s.length; i++) {
+    if (arrre.length === 0) {
+      if (arr.length === 0) arr.push(s[i]);
+      else if (s[i] !== arr[arr.length - 1]) arr.push(s[i]);
+      else {
+        testindex = arr.length - 1;
+        arrre.push(s[i]);
+        if (testindex === 0) {
+          arr = [];
+          arrre = [];
+          testindex = -1;
         }
-        
+      }
+    } else {
+      testindex--;
+      if (s[i] === arr[testindex]) {
+        arrre.push(s[i]);
+        console.log(arrre);
+      } else {
+        testindex = -1;
+        arr = [...arr, ...arrre, s[i]];
+        arrre = [];
+      }
 
-
+      if (testindex === 0) {
+        arr = [];
+        arrre = [];
+        testindex = -1;
+      }
     }
+  }
+  console.log(arr.length === 0 ? 1 : 0);
 
-    
-    
-    return answer;
+  return arr.length === 0 ? 1 : 0;
 }
+
+solution("baabaa");
+solution("ccdd");
+solution("tsaast");
+solution("cccdd");
+solution("cddcddef");
+solution("ttssdtdrrdtdsstttddtsdds");
--- programmers/algorithm/test17.js
+++ programmers/algorithm/test17.js
@@ -1,14 +1,18 @@
 function solution(n) {
-    let answer = -1;
+  let answer = -1;
 
-    for(let i=0; n>=i*i; i++){
-        if(n===i*i){
-            answer = i*i + 2*i + 1;
-        }
-
+  for (let i = 0; n >= i * i; i++) {
+    if (n === i * i) {
+      answer = i * i + 2 * i + 1;
     }
-    console.log(answer);
+  }
+  console.log(answer);
 }
 
+solution(121);
 
-solution(121)
+function solution2(n) {
+  return Number.isInteger(Math.sqrt(n)) ? Math.pow(n + 1, 2) : -1;
+}
+
+solution2(121);
