790162c84577114d08ace70bf44f1bd3738d65f1
Dolwangsung <tjwnrhkd123456@gmail.com>
2022-06-27 15:14:03 +0900

    bowling

---
+++ programmers/algorithm/test38-1bowling.js
@@ -0,0 +1,15 @@
+function solution(bowling) {
+  let i = 0;
+  let array = [];
+
+  let current = 0;
+
+  while (i < bowling.length) {
+    if (bowling[i] === "X") {
+    } else if (bowling[i] === "/") {
+    } else {
+    }
+  }
+}
+
+solution("XXXX90XX9/8/XXX");
